fb5a3f2438cdc3342a6d9f475e2d7a846f8560ab
spundev <spundev@gmail.com>
2026-01-05 19:10:53 +0100

    Modify default "background" color for Surface - This new color works better with our ModePicker buttons (surfaceBright).

--- app/src/main/java/com/spundev/websearchtextaction/MainActivity.kt
+++ app/src/main/java/com/spundev/websearchtextaction/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
@@ -32,7 +33,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WebSearchTextActionTheme {
-                Surface {
+                Surface(
+                    color = MaterialTheme.colorScheme.surfaceContainer
+                ) {
                     NavProvider()
                 }
             }
