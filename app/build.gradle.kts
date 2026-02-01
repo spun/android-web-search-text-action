698cd12ceecd79a6b75277b2e81a2db4cad73f8f
spundev <spundev@gmail.com>
2026-02-01 11:44:04 +0100

    Fix AGP9 issues - Remove org.jetbrains.kotlin.android plugin. AGP has Kotlin support built in now. - Disable newDsl until protobuf releases a compatible version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
     alias(libs.plugins.protobuf)
